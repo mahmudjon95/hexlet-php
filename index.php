f0b1589648a172ad5eaf47d32243f4354f953b37
Mahmudjon <mahmudjon.sayfiddinov@gmail.com>
2023-06-25 15:48:22 +0500

    error on runnig index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
-echo "Hello world";
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
